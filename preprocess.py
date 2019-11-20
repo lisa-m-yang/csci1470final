61855d5fea9164f3050cd83e9efed3b50eba9153
rseet <58007720+rseet@users.noreply.github.com>
2019-11-20 15:46:46 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,14 @@
 import tensorflow as tf
 import numpy as np
+import csv
 
 # reads .csv files and converts them into text files
+
+csv_file = raw_input('Enter the name of your input file: ')
+txt_file = raw_input('Enter the name of your output file: ')
+
+with open(txt_file, "w") as my_output_file:
+    with open(csv_file, "r") as my_input_file:
+        [ my_output_file.write(" ".join(row)+'\n') for row in csv.reader(my_input_file)]
+    my_output_file.close()
 
