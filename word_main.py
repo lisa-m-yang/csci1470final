4b8ee71adf7b4717dae065a184c808ce142988d7
rseet <58007720+rseet@users.noreply.github.com>
2019-12-05 15:38:53 -0500

    Create word_main.py

---
+++ word_main.py
@@ -0,0 +1,54 @@
+
+
+import numpy as np
+import tensorflow as tf
+import argparse
+
+import word_preprocess
+
+
+parser = argparse.ArgumentParser(description='Tensorflow Wikitext-2 RNN/LSTM Language Model')
+parser.add_argument('--data', type=str, default='./data',
+                    help='location of the data corpus')
+parser.add_argument('--model', type=str, default='LSTM',
+                    help='type of recurrent net ')
+parser.add_argument('--emsize', type=int, default=200,
+                    help='size of word embeddings')
+parser.add_argument('--nhid', type=int, default=200,
+                    help='number of hidden units per layer')
+parser.add_argument('--nlayers', type=int, default=2,
+                    help='number of layers')
+parser.add_argument('--lr', type=float, default=20,
+                    help='initial learning rate')
+parser.add_argument('--clip', type=float, default=0.25,
+                    help='gradient clipping')
+parser.add_argument('--epochs', type=int, default=40,
+                    help='upper epoch limit')
+parser.add_argument('--batch_size', type=int, default=20, metavar='N',
+                    help='batch size')
+parser.add_argument('--bptt', type=int, default=35,
+                    help='sequence length')
+parser.add_argument('--dropout', type=float, default=0.2,
+                    help='dropout applied to layers (0 = no dropout)')
+parser.add_argument('--tied', action='store_true',
+                    help='tie the word embedding and softmax weights')
+parser.add_argument('--seed', type=int, default=1111,
+                    help='random seed')
+parser.add_argument('--cuda', action='store_true',
+                    help='use CUDA')
+parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+                    help='report interval')
+parser.add_argument('--save', type=str, default='model.pt',
+                    help='path to save the final model')
+parser.add_argument('--onnx-export', type=str, default='',
+                    help='path to export the final model in onnx format')
+                    
+args = parser.parse_args()
+
+###############################################################################
+# Load data
+###############################################################################
+
+corpus = word_preprocess.Corpus(args.data)
+
+def batchify (data, bsz):
