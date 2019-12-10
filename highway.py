30f1b04fd3221d3c9e690cdb9a0dc9c0f8a30768
lisa-m-yang <lisayang3761@gmail.com>
2019-12-09 23:26:41 -0500

    Update highway.py

--- highway.py
+++ highway.py
@@ -1,2 +1,18 @@
 import tensorflow as tf
-import numpy as np
+from tensorflow.keras.layers import Dense
+
+class Highway(tf.keras.Model):
+    def __init__(self, size):
+
+        super(Highway, self).__init__()
+
+        self.size = size
+
+        self.gate = Dense(self.size, activation='sigmoid', input_shape=(self.size,))
+        self.proj = Dense(self.size, activation='relu')
+
+    def call(self, inputs):
+        gate_output = self.gate(inputs)
+        proj_output = self.proj(inputs)
+
+        return gate_output * proj_output + (1 - gate_output) * proj_output
