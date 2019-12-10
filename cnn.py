77985ad507b3d544e95c89c8130ceb8867783c86
lisa-m-yang <lisayang3761@gmail.com>
2019-12-09 23:18:09 -0500

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -3,18 +3,19 @@ import numpy as np
 import tensorflow as tf
 
 class CNN(tf.keras.Model):
-    def __init__(self, filters):
+    def __init__(self, in_channels, out_channels):
 
         super(CNN, self).__init__()
 
-        self.filters = filters
+        self.in_channels = in_channels
+        self.out_channels = out_channels
         self.kernel_size = 5
 
         self.model = Sequential()
-        self.model.add(Conv1D(self.out_channels, self.kernel_size, activation='relu'))
+        self.model.add(Conv1D(self.out_channels, self.kernel_size, activation='relu', input_shape=(None, self.in_channels))
 
 
     def call(self, inputs):
         output = self.model(inputs)
 
-        return output
+        return tf.reduce_max(output, 2)[0]
